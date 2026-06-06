e35ab4fc626d83c94fd5d27834399b063fd1d95a
JO Dev <dev@jo-project.local>
2026-06-06 10:36:12 +0000

    Logo size changed to 320x190

--- modules/game_entergame_custom/entergame_custom.lua
+++ modules/game_entergame_custom/entergame_custom.lua
@@ -74,7 +74,7 @@ function setup()
         logoWidget = g_ui.loadUIFromString([[
 UIWidget
   id: loginLogoWidget
-  size: 280 140
+  size: 320 190
   image-source: /game_entergame_custom/JO_logo
   image-auto-resize: true
   anchors.horizontalCenter: parent.horizontalCenter
